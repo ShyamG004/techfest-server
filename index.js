c63f0d285b3b2b8b3a2c447b1bf094627d5b83a1
DharshiniR04 <2112026@nec.edu.in>
2025-03-14 13:04:14 +0530

    Updated

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ connectDB();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: "https://techfestnec.vercel.app", 
+    methods: ["POST", "GET"],
+    credentials: true
+  }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://techfestnec.vercel.app/"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 app.use(express.json({ limit: '200mb' })); 
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(helmet());
