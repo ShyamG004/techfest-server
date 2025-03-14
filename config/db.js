7bfb264184c3891e460550f1b5542034700104f6
DharshiniR04 <2112026@nec.edu.in>
2025-03-14 13:09:48 +0530

    Updated

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
+const mongoose = require('mongoose');
 
-dotenv.config();
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected');
-    } catch (error) {
-        console.error('Database connection error:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
 };
 
-module.exports=connectDB;
+module.exports = connectDB;
